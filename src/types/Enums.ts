a0a93990f331244dcddbb5e4d7ee6e4a34c07300
MrAlessandro <alessandro.meschi@icloud.com>
2021-09-27 22:11:56 +0200

    Reorganizing types

---
+++ src/types/Enums.ts
@@ -0,0 +1,5 @@
+export const enum MYscrollState {
+  UNSET,
+  TOP,
+  BELOW
+}
--- src/types/Events.ts
+++ src/types/Events.ts
@@ -1,13 +1,3 @@
-import { EscrollState } from '@/types/EscrollState'
-
-/* HOMEPAGE SCROLL STATE CHANGE EVENT */
-export const MYonChangeHomepageScrollStateEventName = 'my-homepage-scrollstate-change'
-
-export class MYChangeScrollStateEvent {
-  scrollState: EscrollState
-
-  constructor (scrollState: EscrollState) {
-    this.scrollState = scrollState
-  }
-}
-/**************************************/
+/* SPLASH SCREEN AND ANIMATION EVENT */
+export const MYonSplashScreenAnimationEndEventName = 'my-homepage-scrollstate-change'
+/*************************************/
