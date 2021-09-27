abf75922af9b47c0de9d3fb06861226a66d6980a
MrAlessandro <alessandro.meschi@icloud.com>
2021-09-27 23:15:07 +0200

    Logo inline hard-coded, removed library vue-inline-svg

--- src/components/MY-Logo.vue
+++ src/components/MY-Logo.vue
@@ -1,27 +1,116 @@
 <template>
   <div class="MY:brand-logo" :class="{'MY\:scroll-top': isHomepageTop}">
-    <inline-svg class="MY:brand-logo:left-bracket" :src="require('../assets/illustrations/logo/MYlogoLeftBracket.svg')"/>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 31.47 121.38" class="MY:brand-logo:left-bracket">
+      <title>LeftBracket</title>
+      <g class="MY:brand-logo:left-bracket:paths:group">
+        <path class="MY:brand-logo:left-bracket:path"
+              d="M25.63,121.36a22.87,22.87,0,0,1-5.8-.83l-.19,0A10.88,10.88,0,0,1,15,117.64c-.11-.12-.22-.23-.32-.35A13.16,13.16,0,0,1,12,112.13a20.24,20.24,0,0,1-.5-2.18,31.31,31.31,0,0,1-.37-3.18c-.08-1.15-.12-2.3-.12-3.46q0-7.91.56-14.75t.57-14.18a18.67,18.67,0,0,0-.5-4.24l0-.13a8.36,8.36,0,0,0-1.8-3.53,8.26,8.26,0,0,0-2-1.65,11.78,11.78,0,0,0-1.77-.9,10.93,10.93,0,0,0-1.22-.41,14.18,14.18,0,0,0-2.33-.44A21.16,21.16,0,0,0,0,63V58.43a20.07,20.07,0,0,0,3.91-.35,11.65,11.65,0,0,0,2.16-.64,12.34,12.34,0,0,0,2-1A7.88,7.88,0,0,0,9.81,54.9a8.47,8.47,0,0,0,1.83-3.53,16.72,16.72,0,0,0,.5-4.09q0-7.62-.57-14.47c-.15-1.83-.27-3.67-.35-5.5Q11,22.69,11,18.07a42.06,42.06,0,0,1,.38-6A21.54,21.54,0,0,1,12,9.24a3.57,3.57,0,0,1,.12-.4,14.84,14.84,0,0,1,1-2.53A11,11,0,0,1,15,3.74a11.11,11.11,0,0,1,1.08-1A11.29,11.29,0,0,1,19.83.85a20.45,20.45,0,0,1,3.7-.7c1-.1,2.09-.15,3.14-.15h4.8V4.09H27.1A14.32,14.32,0,0,0,21.6,5a6.57,6.57,0,0,0-3.18,2.61,8.92,8.92,0,0,0-.47.86,18.52,18.52,0,0,0-1.55,6.27c-.12,1.29-.17,2.58-.17,3.88q0,1.71,0,3.42.07,4.89.39,9.78.44,6.27.43,13.9V46a24.9,24.9,0,0,1-1.55,9.62,8.13,8.13,0,0,1-2.84,3.46,10.09,10.09,0,0,1-2.81,1.34V61a8.53,8.53,0,0,1,5.65,4.94,25.5,25.5,0,0,1,1.55,9.74q0,7.62-.43,13.9t-.42,13.2q0,7.47,2.19,11,1.63,2.64,5.7,3.3a17.51,17.51,0,0,0,3,.22h4.37v4.1h-4.8Z"/>
+      </g>
+    </svg>
     <div class="MY:brand-logo:content">
-      <inline-svg class="MY:brand-logo:content:bar-code" :src="require('../assets/illustrations/logo/MYlogoBarCode.svg')"/>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 430.35 59.19" class="MY:brand-logo:bar-code">
+        <title>BarCode</title>
+        <g class="MY:brand-logo:bar-code:rectangles:group">
+          <rect class="MY:brand-logo:bar-code:rectangle" width="9.03" height="59.19"/>
+          <rect class="MY:brand-logo:bar-code:rectangle" x="18.06" width="9.03" height="59.19"/>
+          <rect class="MY:brand-logo:bar-code:rectangle" x="48.15" width="9.03" height="59.19"/>
+          <rect class="MY:brand-logo:bar-code:rectangle" x="66.21" width="9.03" height="59.19"/>
+          <rect class="MY:brand-logo:bar-code:rectangle" x="102.32" width="9.03" height="59.19"/>
+          <rect class="MY:brand-logo:bar-code:rectangle" x="132.42" width="9.03" height="59.19"/>
+          <rect class="MY:brand-logo:bar-code:rectangle" x="192.6" width="9.03" height="59.19"/>
+          <rect class="MY:brand-logo:bar-code:rectangle" x="210.66" width="9.03" height="59.19"/>
+          <rect class="MY:brand-logo:bar-code:rectangle" x="252.79" width="9.03" height="59.19"/>
+          <rect class="MY:brand-logo:bar-code:rectangle" x="276.87" width="9.03" height="59.19"/>
+          <rect class="MY:brand-logo:bar-code:rectangle" x="288.91" width="9.03" height="59.19"/>
+          <rect class="MY:brand-logo:bar-code:rectangle" x="306.96" width="9.03" height="59.19"/>
+          <rect class="MY:brand-logo:bar-code:rectangle" x="349.09" width="9.03" height="59.19"/>
+          <rect class="MY:brand-logo:bar-code:rectangle" x="373.17" width="9.03" height="59.19"/>
+          <rect class="MY:brand-logo:bar-code:rectangle" x="385.21" width="9.03" height="59.19"/>
+          <rect class="MY:brand-logo:bar-code:rectangle" x="403.26" width="9.03" height="59.19"/>
+          <rect class="MY:brand-logo:bar-code:rectangle" x="30.09" width="3.01" height="59.19"/>
+          <rect class="MY:brand-logo:bar-code:rectangle" x="36.11" width="3.01" height="59.19"/>
+          <rect class="MY:brand-logo:bar-code:rectangle" x="42.13" width="3.01" height="59.19"/>
+          <rect class="MY:brand-logo:bar-code:rectangle" x="60.19" width="3.01" height="59.19"/>
+          <rect class="MY:brand-logo:bar-code:rectangle" x="84.26" width="3.01" height="59.19"/>
+          <rect class="MY:brand-logo:bar-code:rectangle" x="90.28" width="3.01" height="59.19"/>
+          <rect class="MY:brand-logo:bar-code:rectangle" x="96.3" width="3.01" height="59.19"/>
+          <rect class="MY:brand-logo:bar-code:rectangle" x="114.36" width="3.01" height="59.19"/>
+          <rect class="MY:brand-logo:bar-code:rectangle" x="126.4" width="3.01" height="59.19"/>
+          <rect class="MY:brand-logo:bar-code:rectangle" x="144.45" width="3.01" height="59.19"/>
+          <rect class="MY:brand-logo:bar-code:rectangle" x="150.47" width="3.01" height="59.19"/>
+          <rect class="MY:brand-logo:bar-code:rectangle" x="162.51" width="3.01" height="59.19"/>
+          <rect class="MY:brand-logo:bar-code:rectangle" x="174.55" width="3.01" height="59.19"/>
+          <rect class="MY:brand-logo:bar-code:rectangle" x="186.58" width="3.01" height="59.19"/>
+          <rect class="MY:brand-logo:bar-code:rectangle" x="204.64" width="3.01" height="59.19"/>
+          <rect class="MY:brand-logo:bar-code:rectangle" x="222.7" width="3.01" height="59.19"/>
+          <rect class="MY:brand-logo:bar-code:rectangle" x="234.74" width="3.01" height="59.19"/>
+          <rect class="MY:brand-logo:bar-code:rectangle" x="240.75" width="3.01" height="59.19"/>
+          <rect class="MY:brand-logo:bar-code:rectangle" x="246.77" width="3.01" height="59.19"/>
+          <rect class="MY:brand-logo:bar-code:rectangle" x="270.85" width="3.01" height="59.19"/>
+          <rect class="MY:brand-logo:bar-code:rectangle" x="300.94" width="3.01" height="59.19"/>
+          <rect class="MY:brand-logo:bar-code:rectangle" x="319" width="3.01" height="59.19"/>
+          <rect class="MY:brand-logo:bar-code:rectangle" x="331.04" width="3.01" height="59.19"/>
+          <rect class="MY:brand-logo:bar-code:rectangle" x="337.06" width="3.01" height="59.19"/>
+          <rect class="MY:brand-logo:bar-code:rectangle" x="343.08" width="3.01" height="59.19"/>
+          <rect class="MY:brand-logo:bar-code:rectangle" x="361.13" width="3.01" height="59.19"/>
+          <rect class="MY:brand-logo:bar-code:rectangle" x="397.25" width="3.01" height="59.19"/>
+          <rect class="MY:brand-logo:bar-code:rectangle" x="421.32" width="3.01" height="59.19"/>
+          <rect class="MY:brand-logo:bar-code:rectangle" x="427.34" width="3.01" height="59.19"/>
+        </g>
+      </svg>
       <div class="MY:brand-logo:content:text">
-        <inline-svg class="MY:brand-logo:content:text:web"
-                    :src="require('../assets/illustrations/logo/MYlogoTextWeb.svg')"/>
-        <inline-svg class="MY:brand-logo:content:text:done"
-                    :src="require('../assets/illustrations/logo/MYlogoTextDone.svg')"/>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 112.73 23.34" class="MY:brand-logo:text-web">
+          <title>TextWeb</title>
+          <g class="MY:brand-logo:text-web:letters:group">
+            <polygon class="MY:brand-logo:text-web:letter:W"
+                     points="3.71 23.31 0 0.04 2.41 0.04 4.92 15.79 8.73 1.75 9.53 1.75 13.24 15.89 15.35 0.04 17.55 0.04 13.94 23.31 12.94 23.31 8.93 8.16 4.82 23.31 3.71 23.31"/>
+            <polygon class="MY:brand-logo:text-web:letter:E"
+                     points="49.76 23.31 49.76 0.04 64 0.04 64 2.35 52.16 2.35 52.16 9.97 61.99 9.97 61.99 12.38 52.16 12.38 52.16 21 63.9 21 63.9 23.31 49.76 23.31"/>
+            <path class="MY:brand-logo:text-web:letter:B"
+                  d="M108.34,10.87c3.86-1.2,4.78-7,1.5-9.33C107.1-.73,100.9.24,97.51,0V23.31c3.47-.1,10.66.62,13-1.7C114.37,18.68,112.89,12.17,108.34,10.87ZM100,2.25c3.11,0,9.11-.77,9.23,3.71.46,4-5.91,3.84-9.23,3.71Zm9.88,15.2C109.42,22,103.22,20.88,100,21V11.88C104.13,11.6,111,11.75,109.89,17.45Z"/>
+          </g>
+        </svg>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 158.9 23.88" class="MY:brand-logo:text-done">
+          <title>TextDone</title>
+          <g class="MY:brand-logo:text-done:letters:group">
+            <path class="MY:brand-logo:text-done:letter:N"
+                  d="M96.2,23.42V.14h2.51l9.73,17.26V.14h2.91v.3c-1.1.07,0,22.18-.4,23h-2L98.61,5.16V23.42Z"/>
+            <polygon class="MY:brand-logo:text-done:letter:E"
+                     points="144.65 23.42 144.65 0.14 158.9 0.14 158.9 2.45 147.06 2.45 147.06 10.07 156.89 10.07 156.89 12.48 147.06 12.48 147.06 21.11 158.8 21.11 158.8 23.42 144.65 23.42"/>
+            <path class="MY:brand-logo:text-done:letter:D"
+                  d="M15,9.65C14.53.55,7.41-.47,0,.14V23.42C10.38,24.67,16.09,20.39,15,9.65ZM2.51,21.41V2.35c6.62-1.07,10.53,3.23,10,9.73C13,18.94,8.42,22.24,2.51,21.41Z"/>
+            <path class="MY:brand-logo:text-done:letter:O"
+                  d="M58.37.56C45.7-3.8,43.21,19.46,52.78,23.4,65.09,27.4,67.72,5,58.37.56Zm-2.9,21c-7.93-.38-7.77-18.58,0-19.16C63.2,2.67,63.29,21.3,55.47,21.51Z"/>
+          </g>
+        </svg>
       </div>
     </div>
-    <inline-svg class="MY:brand-logo:right-bracket" :src="require('../assets/illustrations/logo/MYlogoRightBracket.svg')"/>
+    <!--    <inline-svg class="MY:brand-logo:left-bracket" :src="require('../assets/illustrations/logo/MYlogoLeftBracket.svg')"/>
+        <div class="MY:brand-logo:content">
+          <inline-svg class="MY:brand-logo:content:bar-code" :src="require('../assets/illustrations/logo/MYlogoBarCode.svg')"/>
+          <div class="MY:brand-logo:content:text">
+            <inline-svg class="MY:brand-logo:content:text:web"
+                        :src="require('../assets/illustrations/logo/MYlogoTextWeb.svg')"/>
+            <inline-svg class="MY:brand-logo:content:text:done"
+                        :src="require('../assets/illustrations/logo/MYlogoTextDone.svg')"/>
+          </div>
+        </div>
+        <inline-svg class="MY:brand-logo:right-bracket" :src="require('../assets/illustrations/logo/MYlogoRightBracket.svg')"/>-->
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 31.47 121.38" class="MY:brand-logo:right-bracket">
+      <title>RightBracket</title>
+      <g class="MY:brand-logo:right-bracket:paths:group">
+        <path class="MY:brand-logo:right-bracket:path"
+              d="M4.8,121.38H0v-4.1H4.38a17.39,17.39,0,0,0,3-.22q4.07-.66,5.71-3.3,2.17-3.54,2.18-11,0-6.91-.42-13.2t-.42-13.9A25.31,25.31,0,0,1,16,65.91,8.52,8.52,0,0,1,21.59,61v-.56a10.23,10.23,0,0,1-2.81-1.34A8,8,0,0,1,16,55.61,24.9,24.9,0,0,1,14.4,46v-.26q0-7.62.42-13.9.33-4.89.4-9.78c0-1.14,0-2.28,0-3.42a38.61,38.61,0,0,0-.17-3.88,18.29,18.29,0,0,0-1.55-6.27,8.84,8.84,0,0,0-.46-.86A6.61,6.61,0,0,0,9.88,5a14.36,14.36,0,0,0-5.5-.92H0V0H4.8c1,0,2.1.05,3.14.15a20.28,20.28,0,0,1,3.7.7,11.2,11.2,0,0,1,3.79,1.91,9.18,9.18,0,0,1,1.08,1,11,11,0,0,1,1.82,2.57,15.72,15.72,0,0,1,1,2.53l.12.4a23.21,23.21,0,0,1,.6,2.87,40.62,40.62,0,0,1,.38,6q0,4.62-.21,9.24c-.08,1.83-.2,3.67-.35,5.5q-.57,6.86-.56,14.47a16.72,16.72,0,0,0,.49,4.09,8.47,8.47,0,0,0,1.83,3.53,8.17,8.17,0,0,0,1.73,1.49,11.71,11.71,0,0,0,4.17,1.69,20.14,20.14,0,0,0,3.91.35V63a21.27,21.27,0,0,0-2.52.13,14.06,14.06,0,0,0-2.32.44,10.43,10.43,0,0,0-1.23.41,12.27,12.27,0,0,0-1.77.9,8.46,8.46,0,0,0-2,1.65A8.36,8.36,0,0,0,19.86,70l0,.13a18.65,18.65,0,0,0-.49,4.24q0,7.34.56,14.18t.56,14.75c0,1.16,0,2.31-.11,3.46A31.19,31.19,0,0,1,20,110c-.13.74-.29,1.46-.49,2.18a13.43,13.43,0,0,1-2.64,5.16l-.33.35a10.82,10.82,0,0,1-4.68,2.84l-.19,0a22.87,22.87,0,0,1-5.8.83Z"/>
+      </g>
+    </svg>
+
   </div>
 </template>
 
 <script lang="ts">
 import {defineComponent} from "vue";
-import InlineSvg from 'vue-inline-svg';
 
 export default defineComponent({
-  components: {
-    InlineSvg
-  },
   props: {
     isHomepageTop: {
       type: Boolean,
@@ -55,7 +144,7 @@ export default defineComponent({
   .MY\:brand-logo\:content {
     width: 86%;
 
-    .MY\:brand-logo\:content\:bar-code {
+    .MY\:brand-logo\:bar-code {
       fill: $MY-color-primary;
     }
 
@@ -66,11 +155,11 @@ export default defineComponent({
       padding-right: 3.72%;
       padding-top: 1.8%;
 
-      .MY\:brand-logo\:content\:text\:web {
+      .MY\:brand-logo\:text-web {
         fill: $MY-color-primary;
         width: 26.04%;
       }
-      .MY\:brand-logo\:content\:text\:done {
+      .MY\:brand-logo\:text-done {
         fill: $MY-color-primary;
         width: 36.74%;
       }
